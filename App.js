df2f0767ab2906e4b59eab4f4e7c7f8563aeb6ec
aminmansoori <48173745+aminmansoori@users.noreply.github.com>
2020-12-09 04:02:35 -0800

    Update App.js

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { View } from 'react-native';
 import MainScreen from './src/Screens/MainScreen';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
@@ -10,9 +9,7 @@ const App = () => {
 
   return (
     <Provider store={store}>
-      <View style={{ flex: 1, justifyContent: 'flex-end', backgroundColor: "#e6e6ff" }}>
         <MainScreen />
-      </View>
     </Provider>
   )
 }
