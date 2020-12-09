b0194d78032ca2faecbe35687bb331cf183bd674
aminmansoori <48173745+aminmansoori@users.noreply.github.com>
2020-12-09 04:03:06 -0800

    Update MainScreen.js

--- src/Screens/MainScreen.js
+++ src/Screens/MainScreen.js
@@ -1,68 +1,80 @@
-import React, { useState } from 'react';
-import { View, FlatList } from 'react-native';
-import ReceivedChat from '../Components/ReceivedChat';
-import SentChat from '../Components/SentChat';
-import MessageInput from '../Components/MessageInput';
+import React, { useState } from "react";
+import { View, FlatList } from "react-native";
+import ReceivedChat from "./Components/ReceivedChat";
+import SentChat from "./Components/SentChat";
+import MessageInput from "./Components/MessageInput";
 
-const MainScreen = () => {
+const App = () => {
+  let listViewRef; //to manage flatlist scrolling
 
-    let listViewRef; //to manage flatlist scrolling
-
-    //the json array to show some default messages
-    const [messages, setMessage] = useState(
-        [
-            { id: "a", user_id: 2, txtChat: "Hey!", timeChat: "7:15" },
-            { id: "b", user_id: 1, txtChat: "Hi, How are you?", timeChat: "7:16" },
-            { id: "c", user_id: 2, txtChat: "Good. i am just reading this \n tutorial on a really cool chat SDK.\n Have you ever heard of\n CometChat?", timeChat: "7:17" },
-        ]
-    )
-
-    //appending a new message object to the current array
-    const sendmessage = (userMessage) => {
-        var t = new Date();
-        var m = t.getMinutes();
-        var h = t.getHours();
-        var time = h + ":" + m;
-        var new_obj = { id: "r", user_id: 1, txtChat: userMessage, timeChat: time };
-        setMessage([...messages, new_obj]);
+  //the json array to show some default messages
+  const [messages, setMessage] = useState([
+    { id: "a", user_id: 2, txtChat: "Hey!", timeChat: "7:15" },
+    { id: "b", user_id: 1, txtChat: "Hi, How are you?", timeChat: "7:16" },
+    {
+      id: "c",
+      user_id: 2,
+      txtChat:
+        "Good. i am just reading this \n tutorial on a really cool chat SDK.\n Have you ever heard of\n CometChat?",
+      timeChat: "7:17"
     }
+  ]);
 
-    //managing flatlist scrolling
-    const downButtonHandler = () => {
-        listViewRef.scrollToEnd({ animated: true });
-    };
+  //appending a new message object to the current array
+  const sendmessage = (userMessage) => {
+    var t = new Date();
+    var m = t.getMinutes();
+    var h = t.getHours();
+    var time = h + ":" + m;
+    var new_obj = { id: "r", user_id: 1, txtChat: userMessage, timeChat: time };
+    setMessage([...messages, new_obj]);
+  };
 
-    return (
-        <>
-            <View style={{ flex: 1 }} >
-                <FlatList
-                    ref={(ref) => {
-                        listViewRef = ref;
-                    }}
-                    //when the content of the flatlist is changed the function will be fired
-                    onContentSizeChange={() => {
-                        downButtonHandler()
-                    }}
-                    data={messages}
-                    renderItem={({ item }) => {
-                        if (item.user_id == 1) {
-                            return (<SentChat txtChat={item.txtChat} timeChat={item.timeChat} avatar={item.avatar} />)
-                        }
-                        else {
-                            return (<ReceivedChat txtChat={item.txtChat} timeChat={item.timeChat} />)
-                        }
-                    }}
-                    keyExtractor={(item, index) => index.toString()}
+  //managing flatlist scrolling
+  const downButtonHandler = () => {
+    listViewRef.scrollToEnd({ animated: true });
+  };
+
+  return (
+    <View style={{ flex: 1, backgroundColor: "#e6e6ff" }}>
+      <View style={{ flex: 1, paddingTop: 10 }}>
+        <FlatList
+          ref={(ref) => {
+            listViewRef = ref;
+          }}
+          //when the content of the flatlist is changed the function will be fired
+          onContentSizeChange={() => {
+            downButtonHandler();
+          }}
+          data={messages}
+          renderItem={({ item }) => {
+            if (item.user_id === 1) {
+              return (
+                <SentChat
+                  txtChat={item.txtChat}
+                  timeChat={item.timeChat}
+                  avatar={item.avatar}
                 />
-            </View>
-            {
-                //recieving the new message from the child component in order to be saved in the current array
-                <MessageInput sendMessage={(message) => {
-                    sendmessage(message.trim())
-                }} />
+              );
+            } else {
+              return (
+                <ReceivedChat txtChat={item.txtChat} timeChat={item.timeChat} />
+              );
             }
-        </>
-    )
-}
+          }}
+          keyExtractor={(item, index) => index.toString()}
+        />
+      </View>
+      {
+        //recieving the new message from the child component in order to be saved in the current array
+        <MessageInput
+          sendMessage={(message) => {
+            sendmessage(message.trim());
+          }}
+        />
+      }
+    </View>
+  );
+};
 
-export default MainScreen;
+export default App;
