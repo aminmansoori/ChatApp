88c02df3210d5ff3ccafa2e040a1830e2fd51bcb
aminmansoori <48173745+aminmansoori@users.noreply.github.com>
2020-12-09 04:01:46 -0800

    Update SentChat.js

--- src/Components/SentChat.js
+++ src/Components/SentChat.js
@@ -1,51 +1,45 @@
-import React from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
-//get dimentions of the main screen to have a responsive page
-const wp = Dimensions.get("window").width;
-const hp = Dimensions.get("window").height;
-const window = Dimensions.get("window");
-console.log('window : ', window);
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 //recieved props to manage the component due to its parents requierments
 const SentChat = ({ txtChat, timeChat }) => {
-    return (
-        <View style={styles.mainView}>
-            <View style={styles.containerView}>
-                <Text style={styles.chatText}> {txtChat} </Text>
-                <Text style={styles.timeText}>
-                    {timeChat} </Text>
-            </View>
-        </View>
-    );
-}
+  return (
+    <View style={styles.mainView}>
+      <View style={styles.containerView}>
+        <Text style={styles.chatText}>{txtChat}</Text>
+        <Text style={styles.timeText}>{timeChat}</Text>
+      </View>
+    </View>
+  );
+};
 
 export default SentChat;
 
 const styles = StyleSheet.create({
-    mainView: {
-        flex: 1,
-        alignItems: "flex-end",
-        padding: wp * 0.02
-    },
-    containerView: {
-        backgroundColor: "#fff",
-        padding: 0,
-        justifyContent: "center",
-        borderRadius: 15,
-        maxWidth: wp * 0.8,
-        minWidth: wp * 0.2
-    },
-    chatText: {
-        color: "#000",
-        padding: wp * 0.01,
-        paddingBottom: hp * 0.002,
-        fontSize: 16
-    },
-    timeText: {
-        color: "#aaa",
-        fontSize: hp * 0.015,
-        paddingLeft: wp * 0.02,
-        paddingBottom: hp * 0.002
-    }
-
-})
+  mainView: {
+    flex: 1,
+    alignItems: "flex-end",
+    paddingBottom: 5,
+    paddingHorizontal: 10
+  },
+  containerView: {
+    backgroundColor: "#fff",
+    justifyContent: "center",
+    borderRadius: 15,
+    maxWidth: "80%",
+    minWidth: "20%"
+  },
+  chatText: {
+    color: "#000",
+    padding: 5,
+    paddingBottom: -5,
+    fontSize: 16
+  },
+  timeText: {
+    color: "#aaa",
+    fontSize: 12,
+    paddingLeft: 10,
+    paddingBottom: 5,
+    paddingTop: -5
+  }
+});
